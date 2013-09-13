acc1c606e52feb151c71f0a26c35eba24a3fdc04
Frank Agbeko <frank.agbeko@meltwater.org>
2013-09-13 11:43:28 +0000

    now some changes to greetings- not so grt though

--- Greetings.java
+++ Greetings.java
@@ -1,12 +1,13 @@
 public class Greetings{
 	public static void main(String[] args) {
 		//variables declared..
-		String name = "Agbeko frank";
+		String name = "Agbeko Frank";
 		double height = 1.68;
 		int age = 35;
-		String house no = "MB 127/29- Abetei Lane. Nii Boi Man"
-		String greetings = "Hello " + name + ". So you are " + age + " years old" + " and your height is " + height + "m." + "Your house number is supposed to be "+ house no;
-        int n = 
+		String house_no = "MB 127/29- Abetei Lane. Nii Boi Man";
+		String greetings = "Hello " + name + ". So you are " + age + " years old" + " and your height is " + height + "m." + "Your house number is supposed to be "+ "house_no" + "work";
+        int n = 123;
+        String work = "I have worked as a horse. Somebody sat on my back and i ran quickly to take him up stairs ";
 		System.out.println(greetings);
 	} 
 
