11aca2d22e207f74af79b72b72f4a7f1230dedaa
Frank Agbeko <frank.agbeko@meltwater.org>
2013-09-13 10:22:53 +0000

    i just comit my first git. I dont realy know what i did..

---
+++ Greetings.java
@@ -0,0 +1,13 @@
+public class Greetings{
+	public static void main(String[] args) {
+		//variables declared..
+		String name = "Agbeko frank";
+		double height = 1.68;
+		int age = 35;
+		String house no = "MB 127/29- Abetei Lane. Nii Boi Man"
+		String greetings = "Hello " + name + ". So you are " + age + " years old" + " and your height is " + height + "m." + "Your house number is supposed to be "+ house no;
+        int n = 
+		System.out.println(greetings);
+	} 
+
+}
